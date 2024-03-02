db55056584487f7e975820a4331c3fa47cc5a4fe
k-jun <keijun091221@gmail.com>
2024-03-02 21:01:55 +0900

    feat(all): reset button on top right

--- lib/pomodoro_button.dart
+++ lib/pomodoro_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pomodoro/bloc/pomodoro_event.dart';
 import 'bloc/bloc.dart';
 
 class PomodoroButton extends StatelessWidget {
--- lib/pomodoro_status.dart
+++ lib/pomodoro_status.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import 'bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PomodoroStatus extends StatelessWidget {
   const PomodoroStatus({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final pb = BlocProvider.of<PomodoroBloc>(context);
     return Container(
       alignment: Alignment.topRight,
-      child: const Icon(
-        Icons.settings,
-        color: Colors.white,
-        size: 40,
+      child: IconButton(
+        icon: const Icon(
+          Icons.refresh,
+          color: Colors.white,
+          size: 40,
+        ),
+        onPressed: () {
+          pb.add(EventReset());
+        },
       ),
     );
   }
